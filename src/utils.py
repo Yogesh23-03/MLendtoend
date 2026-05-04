15adb4e2f18bcc5ff71d165c8771f7ba11531056
Yogesh23-03 <s66909287@gmail.com>
2026-05-04 17:21:02 +0530

    8th

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,11 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score
 from src.exception import CustomException
 from src.logger import logging
 import pickle
-
+from sklearn.metrics import r2_score
 def save_object(file_path, obj):
     try:
         logging.info("Saving object")
@@ -15,4 +16,23 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
     except Exception as e:
         logging.info("Error in saving object")
+        raise CustomException(e, sys)
+    
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        logging.info("Evaluating models")
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        logging.info("Error in evaluating models")
         raise CustomException(e, sys)
