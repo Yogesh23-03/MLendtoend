7e6d1f22158e8d1eaa795c2cc6f9e877004fc6df
Yogesh23-03 <s66909287@gmail.com>
2026-05-04 17:49:43 +0530

    hyper

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ from src.exception import CustomException
 from src.logger import logging
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path, obj):
     try:
         logging.info("Saving object")
@@ -19,20 +21,35 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, parameters):
     try:
         logging.info("Evaluating models")
         report = {}
-        for i in range(len(models)):
-            model = list(models.values())[i]
+
+        for model_name, model in models.items():
+
+            param = parameters[model_name]
+
+            gs = GridSearchCV(
+                estimator=model,
+                param_grid=param,
+                cv=3,
+                scoring="r2"
+            )
+
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
-            y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
+
             test_model_score = r2_score(y_test, y_test_pred)
-            train_model_score = r2_score(y_train, y_train_pred)
-            report[list(models.keys())[i]] = test_model_score
+
+            report[model_name] = test_model_score
+
         return report
+
     except Exception as e:
         logging.info("Error in evaluating models")
         raise CustomException(e, sys)
